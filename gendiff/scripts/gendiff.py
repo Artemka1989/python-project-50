3606f8034741dec7c8ab82f927eef5ae6b619259
Artemka1989 <kurnev.ar@yandex.ru>
2023-03-19 21:43:26 +0300

    start of second project

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+def main():
+    print('gendiff -h')
+    print('usage: gendiff [-h] first_file second_file\n')
+    print('Compares two configuration files and shows a difference.\n')
+    print('positional arguments:')
+    print('first_file')
+    print('second_file\n')
+    print('optional arguments:')
+    print('-h, --help            show this help message and exit')
+
+
+if __name__ == '__main__':
+    main()
