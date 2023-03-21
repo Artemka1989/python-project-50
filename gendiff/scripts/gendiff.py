38d505a63205818112ea0ef63774e3b9958119ac
Artemka1989 <kurnev.ar@yandex.ru>
2023-03-21 22:09:19 +0300

    .gitignore add module argparse

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,13 @@
+import argparse
+
+
 def main():
-    print('gendiff -h')
-    print('usage: gendiff [-h] first_file second_file\n')
-    print('Compares two configuration files and shows a difference.\n')
-    print('positional arguments:')
-    print('first_file')
-    print('second_file\n')
-    print('optional arguments:')
-    print('-h, --help            show this help message and exit')
+    parser = argparse.ArgumentParser('Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', type=str, help='set format of output')
+    args = parser.parse_args()
+    print(args.first_file)
 
 
 if __name__ == '__main__':
