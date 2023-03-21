f472c97c43a741798cb5145e1e6c242d37b35304
Artemka1989 <kurnev.ar@yandex.ru>
2023-03-21 22:14:42 +0300

    add description to argumentparser

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,7 +2,7 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser('Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', type=str, help='set format of output')
